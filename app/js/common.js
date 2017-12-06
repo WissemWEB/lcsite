84e37f917bda105f63bfc77e64357b75cb18dc19
Vasyl <vasyl.semikhov@gmail.com>
2017-12-06 12:23:15 +0200

    top nav ver 1.0 based on css

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,16 @@
 $(function() {
-	// $('body').fadeOut();
+
+	// $('.sub-nav ul').hide();
+	// $('.sub-nav>a').hover(
+	// 	function() {
+	// 		var ul = $(this).parent('.sub-nav').children('ul');
+	// 		ul.show(100); 
+	// 	}, function() {
+	// 		$('.sub-nav ul').hover(function(){
+	// 			$(this).show();
+	// 		}, function() {
+	// 			$(this).hide();
+	// 		});
+	// 	});	
+
 });
